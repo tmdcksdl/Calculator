867113dc196a3d8de0f2bfa6cac05f5825bb885e
tmdcksdl <tmdcksdl99@naver.com>
2024-11-15 13:26:54 +0900

    :sparkles: Create operate method

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,29 @@
 package calculator;
 
 public class Calculator {
+
+    // 속성
+
+    // 생성자
+    Calculator () {
+
+    }
+    // 메서드
+    int operate (int num1, int num2, String operator) {
+        int result = 0;
+        if (operator.charAt(0) == '+') {
+            result = num1 + num2;
+        } else if (operator.charAt(0) == '-') {
+            result = num1 - num2;
+        } else if (operator.charAt(0) == '*') {
+            result = num1 * num2;
+        } else if (operator.charAt(0) == '/') {
+            if (num2 == 0){
+                System.out.println("나눗셈 연산에서 분모(num2)에 0이 입력될 수 없습니다.");
+            }
+
+            result = num1 / num2;
+        }
+        return result;
+    }
 }
