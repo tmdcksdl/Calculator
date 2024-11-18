d545a73c66ca1862e11d6f0df6f57870b3ff2915
tmdcksdl <tmdcksdl99@naver.com>
2024-11-19 01:12:11 +0900

    Add operate method

--- CalculatorLv2.java
+++ CalculatorLv2.java
@@ -6,8 +6,30 @@ public class CalculatorLv2 {
 
 
     // 생성자
+    CalculatorLv2 () {
 
+    }
 
     // 메서드
+    public int operate (int firstNumber, int secondNumber, char operator) {
+        int result = 0;
 
+        switch (operator) {
+            case '+' :
+                result = firstNumber + secondNumber;
+                break;
+            case '-' :
+                result = firstNumber - secondNumber;
+                break;
+            case '*' :
+                result = firstNumber * secondNumber;
+                break;
+            case '/' :
+                result = firstNumber / secondNumber;
+                break;
+            default :
+                System.out.println("잘못된 사칙연산 기호입니다. 다시 입력해주세요.");
+        }
+        return result;
+    }
 }
