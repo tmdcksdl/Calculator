d3e1e65a74b1bd500cbe1f2bf7599f10774a3702
tmdcksdl <tmdcksdl99@naver.com>
2024-11-19 01:46:09 +0900

    Add Queue

--- App.java
+++ App.java
@@ -1,14 +1,19 @@
 package calculator;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         // 사용자로부터 콘솔 입력을 받을 수 있도록 Scanner 객체 생성
         Scanner scanner = new Scanner(System.in);
+        // 지역 변수
+        Queue<Integer> resultQueue = new LinkedList<>();
 
         // calculatorLv2 생성자를 통해 인스턴스화
-        CalculatorLv2 calculatorLv2 = new CalculatorLv2();
+        // 객체를 생성할 때 생성자를 통해 resultQueue 전달한다.
+        CalculatorLv2 calculatorLv2 = new CalculatorLv2(resultQueue);
 
         // 첫 번째 정수 입력 받기 전 안내 문구 출력
         System.out.print("첫 번째 정수 입력(0 포함) : ");
@@ -21,16 +26,14 @@ public class App {
         int secondNumber = scanner.nextInt();
 
         // 사칙연산 기호(+, -, *, /) 입력 받기 전 문구
-        System.out.println("사칙연산 기호(+, -, *, /) 입력 : ");
+        System.out.print("사칙연산 기호(+, -, *, /) 입력 : ");
         // 사용자로부터 입력 받은 문자열에서 가장 첫 번째 문자(연산 기호)를 operator 변수에 할당
         char operator = scanner.next().charAt(0);
 
-        // 사칙연산의 결과를 저장할 정수형 변수 result 생성
-        int result = 0;
-
         // 입력 받은 양의 정수와 사칙연산 기호를 사용해서 if 조건문을 통해 계산을 수행하고 결과를 result 변수에 저장
         // operator 메서드를 이용해서 한 문장으로 줄일 수 있다. 코드 가독성을 높이기 위해 사용한다.
-        result = calculatorLv2.operate(firstNumber, secondNumber, operator);
+        int result = calculatorLv2.operate(firstNumber, secondNumber, operator);
 
+        System.out.println("연산 결과 : " + result);
     }
 }
--- CalculatorLv2.java
+++ CalculatorLv2.java
@@ -1,13 +1,18 @@
 package calculator;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class CalculatorLv2 {
 
-    // 속성(필드)
+    // 속성 (필드)
+    // 클래스 멤버 변수, 선언만 하고 초기화는 하지 않은 상태.
+    // 계산기의 사칙연산 결과를 저장하는 queue, 해당 클래스 내에서만 사용 가능. 여러 메서드에 사용 가능.
+    private Queue<Integer> queue;
 
-
-    // 생성자
-    CalculatorLv2 () {
-
+    // 생성자 (조립설명서)
+    CalculatorLv2 (Queue<Integer> queue) {
+        this.queue = queue;  // 생성자를 통해 초기화! resultQueue가 할당된다.
     }
 
     // 메서드
