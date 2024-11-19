9b2d7e4c8ed940fbfc80a1c2844cb02b477291c5
tmdcksdl <tmdcksdl99@naver.com>
2024-11-19 14:24:49 +0900

    Fix: Handle division by zero issue

--- App.java
+++ App.java
@@ -33,12 +33,22 @@ public class App {
             // 사용자로부터 입력 받은 문자열에서 가장 첫 번째 문자(연산 기호)를 operator 변수에 할당
             char operator = scanner.next().charAt(0);
 
+            // 나눗셈 연산 시 분모에 0 입력되어 있을 경우 처음으로 돌아가게 설정
+            // if 조건문으로 현재 사용자가 입력한 값 확인 후 결정
+            if (secondNumber == 0 && operator == '/') {
+                System.out.println("나눗셈 연산에서 분모(두 번째 정수)에 0이 입력될 수 없습니다.");
+                continue;
+            }
+
             // 입력 받은 양의 정수와 사칙연산 기호를 사용해서 if 조건문을 통해 계산을 수행하고 결과를 result 변수에 저장
-            // operator 메서드를 이용해서 한 문장으로 줄일 수 있다. 코드 가독성을 높이기 위해 사용한다.
+            // operator 메서드를 이용해서 한 문장으로 줄일 수 있다. 코드 가독성을 높이기 위해 메서드를 생성해서 진행한다.
             int result = calculatorLv2.operate(firstNumber, secondNumber, operator);
 
             // 사칙연산 결과 출력
             System.out.println("연산 결과 : " + result);
+
+            // 구분선 추가
+            System.out.println("--------------------------------------------");
 
             // 사칙연산 결과를 저장할지 물어보는 안내 문구 출력
             System.out.println("결과를 저장하시겠습니까?");
@@ -50,10 +60,13 @@ public class App {
             // 결과를 저장한 후 큐에 저장되어 있는 값 출력
             if (answerNumber == 1) {
                 calculatorLv2.setQueue(result);
-                System.out.println(calculatorLv2.getQueue());
+                System.out.println("현재 저장된 값 : " + calculatorLv2.getQueue());
             } else {
-                System.out.println(calculatorLv2.getQueue());
+                System.out.println("현재 저장된 값 : " + calculatorLv2.getQueue());
             }
+
+            // 구분선 추가
+            System.out.println("--------------------------------------------");
 
             // 큐에 저장된 값을 삭제할지 물어보는 안내 문구 출력
             System.out.println("데이터를 삭제하시겠습니까?");
@@ -65,12 +78,15 @@ public class App {
             // 데이터를 삭제한 후 큐에 저장되어 있는 값 출력
             if (answerNumber2 == 1) {
                 calculatorLv2.pollQueue();
-                System.out.println(calculatorLv2.getQueue());
+                System.out.println("현재 저장된 값 : " + calculatorLv2.getQueue());
             }
 
 
             // 프로그램 종료 부분에서 바로 넘어가는 문제 해결
             scanner.nextLine();
+
+            // 구분선 추가
+            System.out.println("--------------------------------------------");
 
             // 종료를 물어보는 안내 문구 출력
             System.out.print("프로그램을 종료하시겠습니까? (no-계속 진행 | exit-종료) : ");
@@ -84,6 +100,9 @@ public class App {
                 // break문을 사용해서 반복문을 종료하고 프로그램을 종료
                 break;
             }
+
+            // 구분선 추가
+            System.out.println("=============================================");
         }
     }
 }
